fe1a617af7acb13bc255114c93217d84f47717e2
Janilson Varela <janilsonjvs@gmail.com>
2022-05-02 14:16:37 -0300

    Model e Migration ManifestoContratante

---
+++ app/Models/ManifestoContratante.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ManifestoContratante extends Model
+{
+    use HasFactory;
+    public $fillable = ['manifesto_id','cpfcnpj'];
+
+    public function getCpfcnpjAttribute($value)
+    {
+        return Funcoes::formatCPFCNPJ($value);
+    }
+}
