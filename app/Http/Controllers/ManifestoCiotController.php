7f85ae784b2414e2b2486f6b119bd8aa54fa66ab
Janilson Varela <janilsonjvs@gmail.com>
2022-05-08 19:54:58 -0300

    Repository Ciot

--- app/Http/Controllers/ManifestoCiotController.php
+++ app/Http/Controllers/ManifestoCiotController.php
@@ -2,142 +2,31 @@
 
 namespace App\Http\Controllers;
 
-
-use App\Models\ManifestoCiot;
+use App\Repositories\CiotRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Symfony\Component\HttpFoundation\Response;
 
 class ManifestoCiotController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+
+    private $repository;
+
+    public function __construct(CiotRepository $repository)
     {
-        //
+        $this->repository = $repository;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        $validationData = Validator::make($request->all(), [
-            'ciot' => 'required',
-            'cpfcnpj' => 'required',
-            'manifesto_id' => 'required'
-        ]);
-
-        if ($validationData->fails()) {
-            return response()->json([
-                'inserted' => false,
-                'msg' => 'Erro de validação',
-                'errors' =>  $validationData->errors()
-            ], Response::HTTP_BAD_REQUEST);
-        }
-
-        $data = $request->only('manifesto_id', 'ciot', 'cpfcnpj');
-
-        $find = ManifestoCiot::where('ciot', $data['ciot'])
-            ->where('manifesto_id', $data['manifesto_id'])
-            ->first();
-
-        if (isset($find)) {
-            return response()->json(
-                [
-                    'msg' => 'Ciot já lançado'
-                ],
-                Response::HTTP_INTERNAL_SERVER_ERROR
-            );
-        }
-
-
-        try {
-            $create = ManifestoCiot::create($data);
-            return response()->json(
-                [
-                    'inserted' => true,
-                    'data' => $create
-                ],
-                Response::HTTP_CREATED
-            );
-        } catch (\Exception $e) {
-            return response()->json(
-                [
-                    'inserted' => false,
-                    'msg' => env('APP_DEBUG') == true ? 'Error ao inserir: ' . $e->getMessage() : 'Error ao inserir'
-                ],
-                Response::HTTP_INTERNAL_SERVER_ERROR
-            );
-        }
+       return $this->repository->store($request);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        $reg = ManifestoCiot::find($id);
-
-        if ( !isset($reg)) {
-            return response()->json(
-                [
-                    'msg'=> 'Registro não encontrado.',
-                    'deleted' => false
-                ],
-                Response::HTTP_NOT_FOUND
-            );
-        }
-
-        try {
-            $reg->delete();
-            return response()->json(
-                    [
-                        'msg'=> 'Removido com sucesso.',
-                        'deleted' => true
-                    ],
-                    Response::HTTP_OK
-                );
-        } catch (\Exception $e) {
-            return response()->json(
-                [
-                    'msg'=> env('APP_DEBUG') == true ? 'Error ao deletar: ' . $e->getMessage() : 'Error ao deletar',
-                    'deleted' => false
-                ],
-                Response::HTTP_INTERNAL_SERVER_ERROR
-            );
-        }
+       return $this->repository->destroy($id);
     }
 }
---
+++ app/Repositories/CiotRepository.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Http\Request;
+use App\Models\ManifestoCiot;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class CiotRepository extends Repository
+{
+    public function __construct(ManifestoCiot $model)
+    {
+        parent::__construct($model);
+    }
+
+    public function store(Request $request)
+    {
+        $validationData = Validator::make($request->all(), [
+            'ciot' => 'required',
+            'cpfcnpj' => 'required',
+            'manifesto_id' => 'required'
+        ]);
+
+        if ($validationData->fails()) {
+            return response()->json([
+                'inserted' => false,
+                'msg' => 'Erro de validação',
+                'errors' =>  $validationData->errors()
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $data = $request->only('manifesto_id', 'ciot', 'cpfcnpj');
+
+        $find = $this->model->where('ciot', $data['ciot'])
+            ->where('manifesto_id', $data['manifesto_id'])
+            ->first();
+
+        if (isset($find)) {
+            return response()->json(
+                [
+                    'msg' => 'Ciot já lançado'
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+
+
+        try {
+            $create = $this->model->create($data);
+            return response()->json(
+                [
+                    'inserted' => true,
+                    'data' => $create
+                ],
+                Response::HTTP_CREATED
+            );
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'inserted' => false,
+                    'msg' => env('APP_DEBUG') == true ? 'Error ao inserir: ' . $e->getMessage() : 'Error ao inserir'
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+}
