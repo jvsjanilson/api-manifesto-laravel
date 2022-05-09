544eec268b5f3262981f82e8c5a454f3dfe07bb5
Janilson Varela <janilsonjvs@gmail.com>
2022-05-09 09:27:29 -0300

    Repository Municipio Descarregamento

--- app/Http/Controllers/ManifestoMunicipioDescarregamentoController.php
+++ app/Http/Controllers/ManifestoMunicipioDescarregamentoController.php
@@ -2,139 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ManifestoMunicipioDescarregamento;
+use App\Repositories\MunicipioDescarregamentoRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
-use Symfony\Component\HttpFoundation\Response;
-use App\Constantes\Limite;
 
 class ManifestoMunicipioDescarregamentoController extends Controller
 {
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    private $repository;
+
+    public function __construct(MunicipioDescarregamentoRepository $repository)
     {
-        $validationData = Validator::make($request->all(), [
-            'estado_id' => 'required',
-            'municipio_id' => 'required',
-            'manifesto_id' => 'required'
-        ]);
-
-        if ($validationData->fails()) {
-            return response()->json([
-                'inserted' => false,
-                'msg' => 'Erro de validação',
-                'errors' =>  $validationData->errors()
-            ], Response::HTTP_BAD_REQUEST);
-        }
-
-        $data = $request->only('manifesto_id', 'estado_id', 'municipio_id');
-
-        $find = ManifestoMunicipioDescarregamento::where('manifesto_id', $data['manifesto_id'])
-            ->where('estado_id', $data['estado_id'])
-            ->where('municipio_id', $data['municipio_id'])
-            ->first();
-
-        if (isset($find)) {
-            return response()->json(
-                [
-                    'msg' => 'Município já lançado'
-                ],
-                Response::HTTP_INTERNAL_SERVER_ERROR
-            );
-        }
-
-
-        $count = ManifestoMunicipioDescarregamento::select(DB::raw('count(*) as total'))
-            ->where('manifesto_id', $data['manifesto_id'])
-            ->get()[0]['total'];
-
-        if ($count >= Limite::NUMERO_MAXIMO_MUNICIPIO_DESCARREGAMENTO)
-        {
-            return response()->json(
-                [
-                    'msg' => 'Número máximo é ' . strval(Limite::NUMERO_MAXIMO_MUNICIPIO_DESCARREGAMENTO) . '.'
-                ],
-                Response::HTTP_INTERNAL_SERVER_ERROR
-            );
-        }
-
-        try {
-            $create = ManifestoMunicipioDescarregamento::create($data);
-            return response()->json(
-                [
-                    'inserted' => true,
-                    'data' => $create
-                ],
-                Response::HTTP_CREATED
-            );
-        } catch (\Exception $e) {
-            return response()->json(
-                [
-                    'inserted' => false,
-                    'msg' => env('APP_DEBUG') == true ? 'Error ao inserir: ' . $e->getMessage() : 'Error ao inserir'
-                ],
-                Response::HTTP_INTERNAL_SERVER_ERROR
-            );
-        }
+        $this->repository = $repository;
     }
 
+    public function store(Request $request)
+    {
+        return $this->repository->store($request);
+    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        $reg = ManifestoMunicipioDescarregamento::find($id);
-
-        if ( !isset($reg)) {
-            return response()->json(
-                [
-                    'msg'=> 'Registro não encontrado.',
-                    'deleted' => false
-                ],
-                Response::HTTP_NOT_FOUND
-            );
-        }
-
-        try {
-            $reg->delete();
-            return response()->json(
-                    [
-                        'msg'=> 'Removido com sucesso.',
-                        'deleted' => true
-                    ],
-                    Response::HTTP_OK
-                );
-        } catch (\Exception $e) {
-            return response()->json(
-                [
-                    'msg'=> env('APP_DEBUG') == true ? 'Error ao deletar: ' . $e->getMessage() : 'Error ao deletar',
-                    'deleted' => false
-                ],
-                Response::HTTP_INTERNAL_SERVER_ERROR
-            );
-        }
+        return $this->repository->destroy($id);
     }
 }
---
+++ app/Repositories/MunicipioDescarregamentoRepository.php
@@ -0,0 +1,87 @@
+<?php
+
+
+namespace App\Repositories;
+
+use App\Models\ManifestoMunicipioDescarregamento;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+use App\Constantes\Limite;
+
+class MunicipioDescarregamentoRepository extends Repository
+{
+    public function __construct(ManifestoMunicipioDescarregamento $model)
+    {
+        parent::__construct($model);
+    }
+
+    public function store(Request $request)
+    {
+        $validationData = Validator::make($request->all(), [
+            'estado_id' => 'required',
+            'municipio_id' => 'required',
+            'manifesto_id' => 'required'
+        ]);
+
+        if ($validationData->fails()) {
+            return response()->json([
+                'inserted' => false,
+                'msg' => 'Erro de validação',
+                'errors' =>  $validationData->errors()
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $data = $request->only('manifesto_id', 'estado_id', 'municipio_id');
+
+        $find = $this->model->where('manifesto_id', $data['manifesto_id'])
+            ->where('estado_id', $data['estado_id'])
+            ->where('municipio_id', $data['municipio_id'])
+            ->first();
+
+        if (isset($find)) {
+            return response()->json(
+                [
+                    'msg' => 'Município já lançado'
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+
+
+        $count = $this->model->select(DB::raw('count(*) as total'))
+            ->where('manifesto_id', $data['manifesto_id'])
+            ->get()[0]['total'];
+
+        if ($count >= Limite::NUMERO_MAXIMO_MUNICIPIO_DESCARREGAMENTO)
+        {
+            return response()->json(
+                [
+                    'msg' => 'Número máximo é ' . strval(Limite::NUMERO_MAXIMO_MUNICIPIO_DESCARREGAMENTO) . '.'
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+
+        try {
+            $create = $this->model->create($data);
+            return response()->json(
+                [
+                    'inserted' => true,
+                    'data' => $create
+                ],
+                Response::HTTP_CREATED
+            );
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'inserted' => false,
+                    'msg' => env('APP_DEBUG') == true ? 'Error ao inserir: ' . $e->getMessage() : 'Error ao inserir'
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+}
