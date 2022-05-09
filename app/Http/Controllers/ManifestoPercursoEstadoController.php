93904840110803c7deb3438d2a0ff75e6938a59b
Janilson Varela <janilsonjvs@gmail.com>
2022-05-09 09:45:14 -0300

    Repository Percurso Estado

--- app/Http/Controllers/ManifestoPercursoEstadoController.php
+++ app/Http/Controllers/ManifestoPercursoEstadoController.php
@@ -2,121 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ManifestoPercursoEstado;
+use App\Repositories\PercursoEstadoRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Symfony\Component\HttpFoundation\Response;
 
 class ManifestoPercursoEstadoController extends Controller
 {
+    private $repository;
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function __construct(PercursoEstadoRepository $repository)
     {
-        $validationData = Validator::make($request->all(), [
-            'manifesto_id' => 'required',
-            'estado_id' => 'required',
-        ]);
-
-        if ($validationData->fails()) {
-            return response()->json([
-                'inserted' => false,
-                'msg' => 'Erro de validação',
-                'errors' =>  $validationData->errors()
-            ], Response::HTTP_BAD_REQUEST);
-        }
-
-        $data = $request->only('manifesto_id', 'estado_id');
-
-        $find = ManifestoPercursoEstado::where('estado_id', $data['estado_id'])
-            ->where('manifesto_id', $data['manifesto_id'])
-            ->first();
-
-        if (isset($find)) {
-            return response()->json(
-                [
-                    'msg' => 'Estado já lançado'
-                ],
-                Response::HTTP_INTERNAL_SERVER_ERROR
-            );
-        }
-
-        try {
-            $create = ManifestoPercursoEstado::create($data);
-            return response()->json(
-                [
-                    'inserted' => true,
-                    'data' => ManifestoPercursoEstado::join('estados', 'manifesto_percurso_estados.estado_id', '=', 'estados.id')
-                      ->select('estados.uf', 'manifesto_percurso_estados.id')
-                      ->find($create->id)
-                ],
-                Response::HTTP_CREATED
-            );
-        } catch (\Exception $e) {
-            return response()->json(
-                [
-                    'inserted' => false,
-                    'msg' => env('APP_DEBUG') == true ? 'Error ao inserir: ' . $e->getMessage() : 'Error ao inserir'
-                ],
-                Response::HTTP_INTERNAL_SERVER_ERROR
-            );
-        }
+        $this->repository = $repository;
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    public function store(Request $request)
+    {
+        return $this->repository->store($request);
+    }
+
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        $reg = ManifestoPercursoEstado::find($id);
-
-        if ( !isset($reg)) {
-            return response()->json(
-                [
-                    'msg'=> 'Registro não encontrado.',
-                    'deleted' => false
-                ],
-                Response::HTTP_NOT_FOUND
-            );
-        }
-
-        try {
-            $reg->delete();
-            return response()->json(
-                    [
-                        'msg'=> 'Removido com sucesso.',
-                        'deleted' => true
-                    ],
-                    Response::HTTP_OK
-                );
-        } catch (\Exception $e) {
-            return response()->json(
-                [
-                    'msg'=> env('APP_DEBUG') == true ? 'Error ao deletar: ' . $e->getMessage() : 'Error ao deletar',
-                    'deleted' => false
-                ],
-                Response::HTTP_INTERNAL_SERVER_ERROR
-            );
-        }
+        return $this->repository->destroy($id);
     }
 }
---
+++ app/Repositories/PercursoEstadoRepository.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\ManifestoPercursoEstado;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class PercursoEstadoRepository extends Repository
+{
+    public function __construct(ManifestoPercursoEstado $model)
+    {
+        parent::__construct($model);
+    }
+
+    public function store(Request $request)
+    {
+        $validationData = Validator::make($request->all(), [
+            'manifesto_id' => 'required',
+            'estado_id' => 'required',
+        ]);
+
+        if ($validationData->fails()) {
+            return response()->json([
+                'inserted' => false,
+                'msg' => 'Erro de validação',
+                'errors' =>  $validationData->errors()
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $data = $request->only('manifesto_id', 'estado_id');
+
+        $find = $this->model->where('estado_id', $data['estado_id'])
+            ->where('manifesto_id', $data['manifesto_id'])
+            ->first();
+
+        if (isset($find)) {
+            return response()->json(
+                [
+                    'msg' => 'Estado já lançado'
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+
+        try {
+            $create = $this->model->create($data);
+            return response()->json(
+                [
+                    'inserted' => true,
+                    'data' => $this->model->join('estados', 'manifesto_percurso_estados.estado_id', '=', 'estados.id')
+                      ->select('estados.uf', 'manifesto_percurso_estados.id')
+                      ->find($create->id)
+                ],
+                Response::HTTP_CREATED
+            );
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'inserted' => false,
+                    'msg' => env('APP_DEBUG') == true ? 'Error ao inserir: ' . $e->getMessage() : 'Error ao inserir'
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+}
