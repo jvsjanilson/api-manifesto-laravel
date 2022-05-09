ce30fbefcaf19c16f9212f1a1bb1162108ad3d06
Janilson Varela <janilsonjvs@gmail.com>
2022-05-09 10:34:26 -0300

    Repository Municipio

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -2,122 +2,42 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Municipio;
+
+use App\Repositories\MunicipioRepository;
 use Illuminate\Http\Request;
+
 
 class MunicipioController extends Controller
 {
-    private $model;
+    private $repository;
 
-
-    public function __construct(Municipio $model)
+    public function __construct(MunicipioRepository $repository)
     {
-        $this->model = $model;
+        $this->repository = $repository;
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $regs = $this->model->select('id', 'nome', 'estado_id', 'capital', 'cod_ibge')
-            ->paginate(config('app.paginate_number'));
-        return response()->json($regs);
+        return $this->repository->index();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        $data = $request->only('nome', 'estado_id', 'capital', 'cod_ibge');
-        try {
-            $res = $this->model->create($data);
-            return response()->json(['id'=> $res->id],201);
-        } catch (\Exception $e) {
-            return response()->json(
-                [
-                    'code' => $e->getCode(),
-                    'msg'=> $e->getMessage()
-                ]
-                ,500
-            );
-        }
+        return $this->repository->store($request);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        try {
-            $reg = $this->model->find($id);
-            return response()->json($reg, 200);
-
-        } catch (\Exception $e) {
-            return response()->json(
-                [
-                    'code' => $e->getCode(),
-                    'msg'=> $e->getMessage()
-                ]
-                ,500
-            );
-        }
+        return $this->repository->show($id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        $inputs = $request->only('nome', 'capital', 'estado_id', 'cod_ibge');
-
-        try {
-            $reg = $this->model->find($id);
-            $reg->update($inputs);
-            return response()->json(['msg' => 'Atualizado com sucesso.']);
-        } catch (\Exception $e) {
-            return response()->json(
-                [
-                    'code' => $e->getCode(),
-                    'msg'=> $e->getMessage()
-                ]
-                ,500
-            );
-        }
+        return $this->repository->update($request, $id);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        try {
-            $reg = $this->model->find($id);
-            $reg->delete();
-        } catch (\Exception $e) {
-            return response()->json(
-                [
-                    'code' => $e->getCode(),
-                    'msg'=> $e->getMessage()
-                ]
-                ,500
-            );
-        }
+        return $this->repository->destroy($id);
     }
 }
---
+++ app/Repositories/MunicipioRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Municipio;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MunicipioRepository extends Repository
+{
+    public function __construct(Municipio $model)
+    {
+        parent::__construct($model);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->only('nome', 'estado_id', 'capital', 'cod_ibge');
+        try {
+            $res = $this->model->create($data);
+            return response()->json(['id'=> $res->id],Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'code' => $e->getCode(),
+                    'msg'=> $e->getMessage()
+                ]
+                ,Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->only('nome', 'capital', 'estado_id', 'cod_ibge');
+
+        try {
+            $reg = $this->model->find($id);
+            $reg->update($inputs);
+            return response()->json(['msg' => 'Atualizado com sucesso.']);
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'code' => $e->getCode(),
+                    'msg'=> $e->getMessage()
+                ]
+                ,Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+}
