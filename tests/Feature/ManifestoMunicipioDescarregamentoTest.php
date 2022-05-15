681e4377299f1a262df194005511eb3deafd5e55
Janilson Varela <janilsonjvs@gmail.com>
2022-05-15 17:58:10 -0300

    Test Municipio Descarregamento

--- tests/Feature/ManifestoMunicipioDescarregamentoTest.php
+++ tests/Feature/ManifestoMunicipioDescarregamentoTest.php
@@ -2,39 +2,37 @@
 
 namespace Tests\Feature;
 
-use App\Models\ManifestoMunicipioCarregamento;
+use App\Models\ManifestoMunicipioDescarregamento;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ManifestoMunicipioCarregamentoTest extends TestCase
+class ManifestoMunicipioDescarregamentoTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * A basic feature test post.
      *
      * @return void
      */
-    public function test_post_manifesto_municipio_carregamento()
+    public function test_post_manifesto_municipio_descarregamento()
     {
-        $response = $this->post('/api/municipio-carregamentos',[
+        $response = $this->post('/api/municipio-descarregamentos',[
             'manifesto_id' => 1,
             'estado_id' => 12,
             'municipio_id' => 1161,
-
-
         ]);
 
         $response->assertStatus(201);
     }
 
-    public function test_delete_manifesto_municipio_carregamento()
+    public function test_delete_manifesto_municipio_descarregamento()
     {
-        $model = ManifestoMunicipioCarregamento::where('manifesto_id', 1)
+        $model = ManifestoMunicipioDescarregamento::where('manifesto_id', 1)
             ->where('estado_id', 12)
             ->where('municipio_id', 1161)
             ->first();
 
-        $response = $this->delete('/api/municipio-carregamentos/'. $model->id);
+        $response = $this->delete('/api/municipio-descarregamentos/'. $model->id);
         $response->assertStatus(200);
 
     }
