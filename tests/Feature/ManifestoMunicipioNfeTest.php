718874eb5cd78603bad51c6b93a5b7f18edbe2f3
Janilson Varela <janilsonjvs@gmail.com>
2022-05-15 18:09:30 -0300

    Test endpoint Manifesto NFe

---
+++ tests/Feature/ManifestoMunicipioNfeTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\ManifestoNfe;
+use Tests\TestCase;
+
+class ManifestoMunicipioNfeTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_post_manifesto_nfe()
+    {
+        $response = $this->post('/api/nfes',[
+            'manifesto_id' => 1,
+            'municipio_id' => 1161,
+            'chave' => '24232569982423256998242325699824232569981234',
+
+
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+    public function test_delete_manifesto_nfe()
+    {
+        $model = ManifestoNfe::where('manifesto_id', 1)
+            ->where('municipio_id', 1161)
+            ->where('chave', '24232569982423256998242325699824232569981234')
+            ->first();
+
+        $response = $this->delete('/api/nfes/'. $model->id);
+        $response->assertStatus(200);
+
+    }
+}
