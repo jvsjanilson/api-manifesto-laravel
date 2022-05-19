2b42dd5760ca5a1f8794f809c3b3d1fe1438dfec
Janilson Varela <janilsonjvs@gmail.com>
2022-05-19 18:09:54 -0300

    Test required fields Condutor

--- tests/Feature/ManifestoCondutorTest.php
+++ tests/Feature/ManifestoCondutorTest.php
@@ -19,7 +19,6 @@ class ManifestoCondutorTest extends TestCase
             'nome' => 'Janilson Varela de Souza',
             'cpf' => '29140433846'
         ]);
-        //dd($response);
         $response->assertStatus(201);
     }
 
@@ -30,6 +29,24 @@ class ManifestoCondutorTest extends TestCase
 
         $response = $this->delete('/api/condutors/'.$model->id);
         $response->assertStatus(200);
+    }
+
+    public function test_post_manifesto_condutor_required_nome_condutor()
+    {
+        $response = $this->post('/api/condutors',[
+            'manifesto_id' => 1,
+            'cpf' => '29140433846'
+        ]);
+        $response->assertStatus(422);
+    }
+
+    public function test_post_manifesto_condutor_required_cpf_condutor()
+    {
+        $response = $this->post('/api/condutors',[
+            'manifesto_id' => 1,
+            'nome' => 'Janilson Varela de Souza',
+        ]);
+        $response->assertStatus(422);
     }
 
 }
